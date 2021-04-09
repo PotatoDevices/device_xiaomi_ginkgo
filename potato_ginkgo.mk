7aa1f15239ff1e7a9b0e265fe63914533f05d9c2
notakbuzz <bistodroid@gmail.com>
2021-04-09 05:23:48 +0000

    ginkgo: Potato-ify

--- potato_ginkgo.mk
+++ potato_ginkgo.mk
@@ -26,10 +26,10 @@ $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 # Inherit some common ArrowOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := arrow_ginkgo
+PRODUCT_NAME := potato_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8
